494e442ec4921930f1ba7d118ffd85785af53580
Ayush Mandal <ayush.mandal11@gmail.com>
2023-07-30 19:24:13 +0530

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import os
-from pymongo import MongoClient
-
-MONGO_HOST = os.environ.get("MONGO_HOST", "mongo")
-MONGO_PORT = int(os.environ.get("MONGO_PORT", 27017))
-MONGO_DB = os.environ.get("MONGO_DB", "corider")
-
-client = MongoClient(host=MONGO_HOST, port=MONGO_PORT)
-db = client[MONGO_DB]
